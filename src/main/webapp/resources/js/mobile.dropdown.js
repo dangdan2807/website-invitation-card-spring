1e15dd179ee697e111710424146b05d7e65eebff
KayJuno <dangdan2807@gmail.com>
2022-04-20 17:11:38 +0700

    update reset project

--- src/main/webapp/resources/js/mobile.dropdown.js
+++
@@ -1,7 +0,0 @@
-if (window.addEventListener) {
-	window.addEventListener('touchmove', function () {});
-	window.addEventListener('load', function () {});
-} else if (window.attachEvent) {
-	window.attachEvent('ontouchmove', function () {});
-	window.attachEvent('onload', function () {});
-}
