e75eb5e4d0b7f1a736de2b58d6b6ee8fcf72ef6b
KayJuno <dangdan2807@gmail.com>
2022-05-08 22:09:28 +0700

    add: new UserController

--- src/main/java/N1/Controller/HomeController.java
+++ src/main/java/N1/Controller/HomeController.java
@@ -1,22 +1,15 @@
 package N1.Controller;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
-import N1.Dto.PayLoadCreateOrder;
-import N1.Dto.SanPhamMua;
 import N1.Service.*;
 import N1.entity.*;
 
@@ -27,19 +20,7 @@ public class HomeController {
 	private SanPhamService sanPhamService;
 
 	@Autowired
-	private NguoiDungService nguoiDungService;
-
-	@Autowired
 	private LoaiSanPhamService loaiSanPhamService;
-
-	@Autowired
-	private HoaDonService hoaDonService;
-
-	@Autowired
-	private GioHangService gioHangService;
-
-	@Autowired
-	private CTHoaDonService ctHoaDonService;
 
 	@Autowired
 	private DanhGiaService danhGiaService;
@@ -106,91 +87,6 @@ public class HomeController {
 		model.addAttribute("isCategoryPage", 0);
 		
 		return "user/login";
-	}
-
-	@RequestMapping({ "/gio-hang", "/cart" })
-	public String showShoppingCartPage(Model model) {
-		List<LoaiSanPham> dsLoaiSanPham = loaiSanPhamService.findAll();
-		model.addAttribute("dsLoaiSanPham", dsLoaiSanPham);
-		model.addAttribute("isCategoryPage", 0);
-		
-		List<SanPham> dsSanPham = new ArrayList<SanPham>();
-		model.addAttribute("dsSanPham", dsSanPham);
-
-		return "user/shopping-cart";
-	}
-
-	@RequestMapping({ "/thanh-toan/{maND}", "/checkout/{maND}" })
-	public String showCheckoutPage(Model model, @PathVariable int maND) {
-		List<LoaiSanPham> dsLoaiSanPham = loaiSanPhamService.findAll();
-		model.addAttribute("dsLoaiSanPham", dsLoaiSanPham);
-		NguoiDung nguoiDung = nguoiDungService.findNguoiDungById(maND);
-		model.addAttribute("nguoiDung", nguoiDung);
-		List<SanPhamMua> dsSanPhamMua = sanPhamService.getSanPhamMua(maND);
-		model.addAttribute("dsSanPhamMua", dsSanPhamMua);
-		double tongTienHang = 0;
-		for (SanPhamMua sanPhamMua : dsSanPhamMua) {
-			tongTienHang += sanPhamMua.getThanhTien();
-		}
-		double giamGia = tongTienHang * 0.05;
-		double tongThanhToan = tongTienHang - tongTienHang * 0.05;
-		model.addAttribute("tongTienHang", tongTienHang);
-//		System.out.println(tongThanhToan);
-		model.addAttribute("giamGia", giamGia);
-		model.addAttribute("tongThanhToan", tongThanhToan);
-
-		model.addAttribute("isCategoryPage", 0);
-		
-		return "user/checkout";
-	}
-
-	@RequestMapping(value = "/orders/success", method = RequestMethod.POST)
-	public String createHoaDon(PayLoadCreateOrder payLoadCreateOrder, Model model) {
-		System.out.println(payLoadCreateOrder.getDiaChi());
-		// 1 Lay user tu context security
-		Integer userId = 19;
-		// 1.1 lay chi tiet user
-		NguoiDung nguoiDung = nguoiDungService.findNguoiDungById(userId);
-		// 2 Lay thong tin gio hang tu user
-		List<ChiTietHoaDon> chiTietHoaDons = new ArrayList<ChiTietHoaDon>();
-		List<SanPhamMua> dsSanPhamMua = sanPhamService.getSanPhamMua(userId);
-		// có danh sách sản phẩm mua -> mã sp, số lượng , thành tiền
-
-		double tongTienHang = 0;
-		int tongSoLuong = 0;
-		for (SanPhamMua sanPhamMua : dsSanPhamMua) {
-			tongTienHang += sanPhamMua.getThanhTien();
-			tongSoLuong += sanPhamMua.getSoLuong();
-		}
-		double tongThanhToan = tongTienHang - tongTienHang * 0.05;
-		// 3 Tien hanh tao hoa don, tao chi tiet hoa don-> Luu thanh cong
-		Date ngayLHD = new Date();
-		Date ngayGiaoHang = new Date(ngayLHD.getTime() + (3 * 1000 * 60 * 60 * 24));
-		String trangThaiDonHang = "Chưa thanh toán";
-		HoaDon hoaDon = new HoaDon(ngayLHD, tongThanhToan, tongSoLuong, trangThaiDonHang, ngayGiaoHang,
-				payLoadCreateOrder.getDiaChi(), nguoiDung);
-		HoaDon hoadonSave = hoaDonService.addHoaDon(hoaDon);
-
-		dsSanPhamMua.forEach(e -> {
-			SanPham sanPham = sanPhamService.getSanPhamByIdSanPham(e.getMaSp());
-			ChiTietHoaDon cthd = new ChiTietHoaDon(hoadonSave, sanPham, e.getSoLuong(), e.getGiaSp());
-			ctHoaDonService.addChiTietHoaDon(cthd);
-			chiTietHoaDons.add(cthd);
-
-		});
-
-	
-		// 4 Xoa gio hang cua khach hang
-		gioHangService.deleteGioHangByIdNguoiDung(userId);
-		// 5 Tao trang chi tiet hoa don( truyen du lieu hoa don vua tao duoc qua trang
-		// do)
-		model.addAttribute("hoadonThanhToan", hoadonSave);
-
-		model.addAttribute("chiTietHoaDons", chiTietHoaDons);
-		model.addAttribute("tongTienHang", tongTienHang);
-		model.addAttribute("giamGia", tongTienHang * 0.05);
-		System.out.println(hoadonSave.toString());
-		return "user/detail-order";
 	}
 
 	@GetMapping("/access-denied")
